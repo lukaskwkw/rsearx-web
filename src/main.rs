4d541594561b7cc527073eb42502a40ddfddf2b4
Łukasz Kwiecień <lukaskwkw@gmail.com>
2022-08-26 22:23:26 +0200

    feat(save) Add save request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 use std::{collections::HashSet, rc::Rc};
 use web_sys::HtmlInputElement;
 
@@ -65,6 +68,12 @@ fn app() -> Html {
             })
         }
     };
+    let onsubmit = {
+        let options_state = options_state.clone();
+        move |_| {
+             options_state.dispatch(Action::SendState);
+        }
+    };
     let grades = options_state.grades.clone();
     let inputs = options_state.text_inputs.clone();
     let search_value = state.search_query.clone();
@@ -73,20 +82,21 @@ fn app() -> Html {
         .then(|| "flex".to_string())
         .unwrap_or_else(|| "none".to_string());
     html!(
-        <div class="w-full justify-center container mx-auto px-4 flex flex-col items-center gap-8">
-            <div style={format!("display: {}",display_options)}>
-                <Options {set_input} {set_grade} {inputs} {grades} />
-            </div>
-            <div class="w-full flex justify-center gap-2">
-                <form class="w-full lg:w-1/2 relative" method="GET" action="/search">
-                    <span class="flex inset-y-0 items-center absolute left-0 pl-2"><Icon icon_id={IconId::FeatherSearch}/></span>
-                    <input class="w-full px-4 py-2 pl-8 border border-slate-700 shadow-sm focus:outline-none focus:border-sky-700"
-                        placeholder="Search" type="text" name="q" {oninput} value={search_value} />
-                </form>
-                <button class="hover:rotate-90 transition-transform" {onclick}><Icon icon_id={IconId::FontAwesomeSolidGears}/></button>
-            </div>
+    <div class="w-full justify-center container mx-auto px-4 flex flex-col items-center gap-8">
+        <div class="flex-col" style={format!("display: {}",display_options)}>
+            <Options {set_input} {set_grade} {inputs} {grades} />
+            <input onclick={onsubmit} class="rounded-lg border bg-slate-300 p-2 active:bg-blue-200" type="submit" value="Save" />
         </div>
-        )
+        <div class="w-full flex justify-center gap-2">
+            <form class="w-full lg:w-1/2 relative" method="GET" action="/search">
+                <span class="flex inset-y-0 items-center absolute left-0 pl-2"><Icon icon_id={IconId::FeatherSearch}/></span>
+                <input class="w-full px-4 py-2 pl-8 border border-slate-700 shadow-sm focus:outline-none focus:border-sky-700"
+                    placeholder="Search" type="text" name="q" {oninput} value={search_value} />
+            </form>
+            <button class="hover:rotate-90 transition-transform" {onclick}><Icon icon_id={IconId::FontAwesomeSolidGears}/></button>
+        </div>
+    </div>
+    )
 }
 
 fn main() {
